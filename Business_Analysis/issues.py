e90eb89b9fd66872a8b476fcdd9a9f0c1a34691e
Ayoub AIT-TAELB <ayoubaittaleb133@gmail.com>
2024-03-15 17:43:56 +0800

    init the user issues

--- Business_Analysis/issues.py
+++ Business_Analysis/issues.py
@@ -36,3 +36,44 @@ result = df.groupBy('name') \
            .orderBy(col('count').desc()) \
            .limit(20)
 z.show(result)
+
+
+#5. Count the number of categories.
+
+
+num_categories = df.select(countDistinct('categories').alias('Number_categories'))
+num_categories.show()
+
+#6. Identify and list the top 10 categories with the highest frequency
+
+
+category_High_fr = df.groupBy('categories') \
+                    .count() \
+                    .orderBy(desc('count')) \
+                    .limit(10)
+z.show(category_High_fr)
+
+#7. List the top 20 merchants with the most five-star reviews.
+
+
+five_star_merchants = df.filter(col('stars') == 5)
+
+top_20 = five_star_merchants.groupBy('name').count()\
+            .orderBy(col('count').desc())\
+            .limit(20)
+top_20.show()
+
+
+#8.Summarize the types and quantities of restaurants for different cuisines
+
+american_summary = american_summary.withColumn('cuisine', lit('American')).limit(30)
+mexican_summary = mexican_summary.withColumn('cuisine', lit('Mexican')).limit(30)
+chinese_summary = chinese_summary.withColumn('cuisine', lit('Chinese')).limit(30)
+
+american_summary = american_restaurants.groupBy('categories').count()
+mexican_summary = mexican_restaurants.groupBy('categories').count()
+chinese_summary = chinese_restaurants.groupBy('categories').count()
+
+z.show(american_summary)
+z.show(mexican_summary)
+z.show(chinese_summary)
